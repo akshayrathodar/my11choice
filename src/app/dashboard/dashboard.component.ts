ffe4a7c579bf74ef94bc1242b3a5cb23891149dc
AkshayNcrypted <akshay.rathod@ncrypted.com>
2021-05-28 18:25:05 +0530

    added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+  
   ngOnInit(): void {
+    
   }
 
+  onToggleClick(id:any)  {
+    if($('.subm'+id).hasClass('hide')) {
+      $('.subm'+id).addClass('show');
+      $('.subm'+id).removeClass('hide');
+    } else {
+      $('.subm'+id).addClass('hide');
+      $('.subm'+id).removeClass('show');
+    }
+    // if($('.subm'+id).css('display') == 'block'){
+    //   $('.sidebar-submenu'+id).css('display','none');
+    // } else {
+    //   $('.sidebar-submenu'+id).css('display','block');
+    // }
+  }
+
+  
 }
